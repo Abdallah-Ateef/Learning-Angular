d73537fb0ac23ff9bd5c08e991f82ef5cbae6bae
Abdallah <abdallahatefelsayed@gamil.com>
2026-04-07 22:49:16 +0200

    Add contactus component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { Contactus } from './contactus/contactus';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'contactus', component: Contactus },
+];
